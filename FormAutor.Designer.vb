ddbcbca092a6046da92c530a34b9eb9e4ce6b194
ovtt17 <ovettmora17@gmail.com>
2023-10-06 18:36:37 -0600

    update: formulario de autor creado

---
+++ FormAutor.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormActor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NombreTxt = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnBorrar = New System.Windows.Forms.Button()
+        Me.BtnEditar = New System.Windows.Forms.Button()
+        Me.BtnGuardar = New System.Windows.Forms.Button()
+        Me.BtnNuevo = New System.Windows.Forms.Button()
+        Me.PaisLbl = New System.Windows.Forms.Label()
+        Me.AutorLbl = New System.Windows.Forms.Label()
+        Me.GridAutor = New System.Windows.Forms.DataGridView()
+        Me.CodigoColumna = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NombreAutorColumna = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PaisColumna = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.GridAutor, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.MidnightBlue
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.NombreTxt)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.PaisLbl)
+        Me.Panel1.Controls.Add(Me.AutorLbl)
+        Me.Panel1.Controls.Add(Me.GridAutor)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(528, 564)
+        Me.Panel1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(123, 373)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(204, 28)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'NombreTxt
+        '
+        Me.NombreTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NombreTxt.Location = New System.Drawing.Point(123, 310)
+        Me.NombreTxt.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.NombreTxt.Name = "NombreTxt"
+        Me.NombreTxt.Size = New System.Drawing.Size(204, 26)
+        Me.NombreTxt.TabIndex = 5
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.BtnSalir)
+        Me.GroupBox1.Controls.Add(Me.BtnBorrar)
+        Me.GroupBox1.Controls.Add(Me.BtnEditar)
+        Me.GroupBox1.Controls.Add(Me.BtnGuardar)
+        Me.GroupBox1.Controls.Add(Me.BtnNuevo)
+        Me.GroupBox1.Location = New System.Drawing.Point(358, 284)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(150, 257)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnSalir.BackColor = System.Drawing.Color.Crimson
+        Me.BtnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSalir.Location = New System.Drawing.Point(24, 194)
+        Me.BtnSalir.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(112, 34)
+        Me.BtnSalir.TabIndex = 4
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        'BtnBorrar
+        '
+        Me.BtnBorrar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnBorrar.BackColor = System.Drawing.Color.Crimson
+        Me.BtnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBorrar.Location = New System.Drawing.Point(24, 154)
+        Me.BtnBorrar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnBorrar.Name = "BtnBorrar"
+        Me.BtnBorrar.Size = New System.Drawing.Size(112, 34)
+        Me.BtnBorrar.TabIndex = 3
+        Me.BtnBorrar.Text = "Borrar"
+        Me.BtnBorrar.UseVisualStyleBackColor = False
+        '
+        'BtnEditar
+        '
+        Me.BtnEditar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnEditar.BackColor = System.Drawing.Color.Gold
+        Me.BtnEditar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEditar.Location = New System.Drawing.Point(24, 111)
+        Me.BtnEditar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnEditar.Name = "BtnEditar"
+        Me.BtnEditar.Size = New System.Drawing.Size(112, 34)
+        Me.BtnEditar.TabIndex = 2
+        Me.BtnEditar.Text = "Editar"
+        Me.BtnEditar.UseVisualStyleBackColor = False
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnGuardar.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnGuardar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnGuardar.Location = New System.Drawing.Point(24, 68)
+        Me.BtnGuardar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(112, 34)
+        Me.BtnGuardar.TabIndex = 1
+        Me.BtnGuardar.Text = "Guardar"
+        Me.BtnGuardar.UseVisualStyleBackColor = False
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnNuevo.BackColor = System.Drawing.Color.Green
+        Me.BtnNuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnNuevo.Location = New System.Drawing.Point(24, 22)
+        Me.BtnNuevo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(112, 34)
+        Me.BtnNuevo.TabIndex = 0
+        Me.BtnNuevo.Text = "Nuevo"
+        Me.BtnNuevo.UseVisualStyleBackColor = False
+        '
+        'PaisLbl
+        '
+        Me.PaisLbl.AutoSize = True
+        Me.PaisLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PaisLbl.Location = New System.Drawing.Point(62, 373)
+        Me.PaisLbl.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.PaisLbl.Name = "PaisLbl"
+        Me.PaisLbl.Size = New System.Drawing.Size(43, 20)
+        Me.PaisLbl.TabIndex = 3
+        Me.PaisLbl.Text = "Pais:"
+        '
+        'AutorLbl
+        '
+        Me.AutorLbl.AutoSize = True
+        Me.AutorLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AutorLbl.Location = New System.Drawing.Point(62, 310)
+        Me.AutorLbl.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.AutorLbl.Name = "AutorLbl"
+        Me.AutorLbl.Size = New System.Drawing.Size(52, 20)
+        Me.AutorLbl.TabIndex = 2
+        Me.AutorLbl.Text = "Autor:"
+        '
+        'GridAutor
+        '
+        Me.GridAutor.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GridAutor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridAutor.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.CodigoColumna, Me.NombreAutorColumna, Me.PaisColumna})
+        Me.GridAutor.Location = New System.Drawing.Point(16, 11)
+        Me.GridAutor.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GridAutor.Name = "GridAutor"
+        Me.GridAutor.ReadOnly = True
+        Me.GridAutor.RowHeadersWidth = 51
+        Me.GridAutor.RowTemplate.Height = 24
+        Me.GridAutor.Size = New System.Drawing.Size(492, 244)
+        Me.GridAutor.TabIndex = 1
+        '
+        'CodigoColumna
+        '
+        Me.CodigoColumna.HeaderText = "Codigo"
+        Me.CodigoColumna.MinimumWidth = 6
+        Me.CodigoColumna.Name = "CodigoColumna"
+        Me.CodigoColumna.ReadOnly = True
+        Me.CodigoColumna.Width = 145
+        '
+        'NombreAutorColumna
+        '
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NombreAutorColumna.DefaultCellStyle = DataGridViewCellStyle1
+        Me.NombreAutorColumna.HeaderText = "Autor"
+        Me.NombreAutorColumna.MinimumWidth = 6
+        Me.NombreAutorColumna.Name = "NombreAutorColumna"
+        Me.NombreAutorColumna.ReadOnly = True
+        Me.NombreAutorColumna.Width = 145
+        '
+        'PaisColumna
+        '
+        Me.PaisColumna.HeaderText = "Pais"
+        Me.PaisColumna.MinimumWidth = 6
+        Me.PaisColumna.Name = "PaisColumna"
+        Me.PaisColumna.ReadOnly = True
+        Me.PaisColumna.Width = 145
+        '
+        'FormActor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 564)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "FormActor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Catálogo de Autor."
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.GridAutor, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GridAutor As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PaisLbl As Label
+    Friend WithEvents AutorLbl As Label
+    Friend WithEvents BtnBorrar As Button
+    Friend WithEvents BtnEditar As Button
+    Friend WithEvents BtnGuardar As Button
+    Friend WithEvents BtnNuevo As Button
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents NombreTxt As TextBox
+    Friend WithEvents CodigoColumna As DataGridViewTextBoxColumn
+    Friend WithEvents NombreAutorColumna As DataGridViewTextBoxColumn
+    Friend WithEvents PaisColumna As DataGridViewTextBoxColumn
+End Class
---
+++ FormAutor.vb
@@ -0,0 +1,13 @@
+﻿Public Class FormActor
+    Private Sub AutorLbl_Click(sender As Object, e As EventArgs) Handles AutorLbl.Click
+
+    End Sub
+
+    Private Sub PaisLbl_Click(sender As Object, e As EventArgs) Handles PaisLbl.Click
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
