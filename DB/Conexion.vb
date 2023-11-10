2ea2cec1c442bc8056029b1330c7060c872ee12b
ovtt17 <ovettmora17@gmail.com>
2023-11-10 12:23:48 -0600

    using localhost

--- DB/Conexion.vb
+++ DB/Conexion.vb
@@ -5,7 +5,7 @@ Imports System.Windows.Forms
 
 Public Class Conexion
 
-    Private Const ConnectionString As String = "Server=127.168.1.19;Database=biblioteca;User ID=Maria_Bustamante;Pwd=Maria1234;"
+    Private Const ConnectionString As String = "Server=localhost;Database=biblioteca;User ID=Maria_Bustamante;Pwd=Maria1234;"
     Public Function Connect() As MySqlConnection
         Dim cx = New MySqlConnection(ConnectionString)
         Try
